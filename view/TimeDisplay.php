1cb3a5cc4857d1dfac3f68290ba54e9d8e6aa4a2
Anna Malmgren <am223rj@student.lnu.se>
2019-12-15 20:14:16 +0100

    adding view

---
+++ view/TimeDisplay.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace View;
+
+class TimeDisplay {
+  
+
+    public function startTime($time) {
+        echo "\nSTART TIME: $time\n";
+    }
+
+    public function endTime($time) {
+        echo "END TIME: $time\n";
+    }
+
+}
